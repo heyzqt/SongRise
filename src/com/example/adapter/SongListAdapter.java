ffc76544024985aa66c3c4750c7f0b562cbfd307
erque <1062631582@qq.com>
2016-09-02 15:36:38 +0800

    acsc

--- src/com/example/adapter/SongListAdapter.java
+++ src/com/example/adapter/SongListAdapter.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -22,7 +23,7 @@ import android.widget.TextView;
  * 
  */
 public class SongListAdapter extends BaseAdapter {
-
+	private ImageView ii;
 	private List<List<Map<String, String>>> songlist;
 	private Context context;
 
